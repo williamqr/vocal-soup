9209510705a6de736c4e698f24db25ce27ee1a8f
Will Zhao <zhaoqirenjisoo@gmail.com>
2026-03-24 14:51:20 -0500

    added memory for login

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,15 @@
 // src/lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SUPABASE_URL = "https://qviplksdhdsfeceuotww.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_yXbgFgMnkcpTYsn-RpLFuA_KwBmvZEr";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
